9431d1c7c61826872b63b4792d8c9a7434bc8a28
hussamahalshawi <hussamahalshawi@gmail.com>
2023-01-23 01:52:44 +0200

    add social app

---
+++ dwitter/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
+
+# Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Only display the "username" field
+    fields = ["username"]
+    inlines = [ProfileInline]
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+# admin.site.register(Profile)
---
+++ dwitter/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True
+    )
+    def __str__(self):
+        return self.user.username
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+# Create a Profile for each new user.
+post_save.connect(create_profile, sender=User)
