c24833b749a3dd48337dfc884fef3ded86a0d8d6
hussamahalshawi <hussamahalshawi@gmail.com>
2023-01-23 16:46:43 +0200

    add templates, urls, views, profile and profile_list

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
@@ -13,6 +14,8 @@ class Profile(models.Model):
     )
     def __str__(self):
         return self.user.username
+
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
@@ -20,4 +23,4 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.follows.set([instance.profile.id])
         user_profile.save()
 # Create a Profile for each new user.
-post_save.connect(create_profile, sender=User)
+# post_save.connect(create_profile, sender=User)
--- dwitter/views.py
+++ dwitter/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def dashboard(request):
+    return render(request, "base.html")
+
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "dwitter/profile_list.html", {"profiles": profiles})
+
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(request, "dwitter/profile.html", {"profile": profile})
