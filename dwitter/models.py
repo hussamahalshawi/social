4b3d79470284808c0be69e6e55ccec8371242ad6
hussamahalshawi <hussamahalshawi@gmail.com>
2023-01-23 15:31:30 +0200

    add follows

--- dwitter/models.py
+++ dwitter/models.py
@@ -17,5 +17,7 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
         user_profile.save()
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
 # Create a Profile for each new user.
 post_save.connect(create_profile, sender=User)
