2636980ac9617e44da13104cacaf6e278db06c0a
hussamahalshawi <hussamahalshawi@gmail.com>
2023-01-23 17:57:19 +0200

    add Dweet on models

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-from .models import Profile
+from .models import Profile, Dweet
 
 # Register your models here.
 class ProfileInline(admin.StackedInline):
@@ -14,3 +14,4 @@ admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
 # admin.site.register(Profile)
+admin.site.register(Dweet)
--- dwitter/models.py
+++ dwitter/models.py
@@ -24,3 +24,9 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.save()
 # Create a Profile for each new user.
 # post_save.connect(create_profile, sender=User)
+class Dweet(models.Model):
+    user = models.ForeignKey(User, related_name="dweets", on_delete=models.DO_NOTHING)
+    body = models.CharField(max_length=140)
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return (f"{self.user} "f"({self.created_at:%Y-%m-%d %H:%M}): "f"{self.body[:30]}...")
