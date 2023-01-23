a874be689a7160f8606b5860f49b87bced3bc4b7
hussamahalshawi <hussamahalshawi@gmail.com>
2023-01-23 17:18:07 +0200

    add follows and unfollows

--- dwitter/views.py
+++ dwitter/views.py
@@ -15,5 +15,17 @@ def profile_list(request):
 
 
 def profile(request, pk):
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
     profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == "unfollow":
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()
     return render(request, "dwitter/profile.html", {"profile": profile})
